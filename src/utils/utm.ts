0204645931fe42abaa25b2dabc3d804fc354b3a1
Clément Murzeau <clement@studiorelief.fr>
2023-11-03 18:13:02 +0100

    build branch-1.5.2
    Cookie local storage

--- src/utils/utm.ts
+++ src/utils/utm.ts
@@ -1,24 +1,25 @@
 function fillUTMParametersFromUrl(): void {
-  // Récupération de l'URL actuelle
   const currentUrl = window.location.href;
   const parsedUrl = new URL(currentUrl);
   const params = parsedUrl.searchParams;
 
-  // Les clés UTM que nous souhaitons extraire
   const utmKeys = ['utm_source', 'utm_medium', 'utm_campaign', 'utm_content', 'utm_term'];
 
-  // Parcourir chaque clé UTM et remplir le champ correspondant si la valeur est présente
   utmKeys.forEach((key) => {
-    const value = params.get(key);
+    let value = params.get(key);
     if (value) {
-      const inputElement = document.querySelector(`input[name="${key}"]`) as HTMLInputElement;
-      if (inputElement) {
-        inputElement.value = value;
-      }
+      // Si une valeur UTM est présente dans l'URL, l'enregistrer dans localStorage
+      localStorage.setItem(key, value);
+    } else {
+      // Sinon, récupérer la valeur UTM précédente de localStorage, si elle existe
+      value = localStorage.getItem(key) || '';
+    }
+    const inputElement = document.querySelector(`input[name="${key}"]`) as HTMLInputElement;
+    if (inputElement && value) {
+      inputElement.value = value;
     }
   });
 
-  // Ajout de la logique pour remplir l'input 'url_form' avec l'URL complète
   const urlFormInputElement = document.querySelector('input[name="url_form"]') as HTMLInputElement;
   if (urlFormInputElement) {
     urlFormInputElement.value = currentUrl;
